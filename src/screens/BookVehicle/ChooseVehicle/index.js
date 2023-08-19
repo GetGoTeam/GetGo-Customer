38b102622d2f7ba8950e465539e54f6627c345cd
Phuoc Dinh <phuocdinh462@gmail.com>
2023-08-19 09:43:49 +0700

    chore: calculate price

--- src/screens/BookVehicle/ChooseVehicle/index.js
+++ src/screens/BookVehicle/ChooseVehicle/index.js
@@ -1,12 +1,14 @@
 import { TouchableOpacity, View } from "react-native";
 import styles from "./styles";
 import ChooseVehicleItem from "~components/ChooseVehicleItem";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { selectVehicleType, setVehicleType } from "~/slices/navSlice";
+import { selectVehicleType, setVehicleType, selectOrigin, selectDestination } from "~/slices/navSlice";
 import { useSelector } from "react-redux";
+import { GOONG_APIKEY } from "@env";
+import axios from "axios";
 
-const chooseVehicleData = [
+const DefaultChooseVehicleData = [
   {
     title: "Xe máy",
     icon: require("~assets/motorcycle.png"),
@@ -27,10 +29,75 @@ const chooseVehicleData = [
   },
 ];
 
+const getDistance = async (oriLat, oriLng, desLat, desLng, vehicle) => {
+  const url = `https://rsapi.goong.io/DistanceMatrix?origins=${oriLat},${oriLng}&destinations=${desLat},${desLng}&vehicle=${vehicle}&api_key=${GOONG_APIKEY}`;
+
+  try {
+    const response = await axios.get(url);
+    if (response.data) {
+      const distance = response.data.rows[0].elements[0].distance.value;
+      return distance;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Error getting place from coordinates:", error);
+    return "Error";
+  }
+};
+
+const roundNumber = (n) => {
+  return Math.round(n / 1000) * 1000 + 1000;
+};
+
 const ChooseVehicle = () => {
   const vehicleType = useSelector(selectVehicleType);
+  const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
   const [chooseIndex, setChooseIndex] = useState(vehicleType === "motorcycle" ? 0 : 1);
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [chooseVehicleData, setChooseVehicleData] = useState(DefaultChooseVehicleData);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const distance = await getDistance(
+          origin.latitude,
+          origin.longitude,
+          destination.latitude,
+          destination.longitude,
+          vehicleType === "motorcycle" ? "bike" : "car"
+        );
+
+        setChooseVehicleData([
+          {
+            title: "Xe máy",
+            icon: require("~assets/motorcycle.png"),
+            price: roundNumber((15000 * distance) / 1000),
+            type: "motorcycle",
+          },
+          {
+            title: "Xe hơi 4 chỗ",
+            icon: require("~assets/car.png"),
+            price: roundNumber((30000 * distance) / 1000),
+            type: "car4",
+          },
+          {
+            title: "Xe hơi 7 chỗ",
+            icon: require("~assets/car.png"),
+            price: roundNumber((50000 * distance) / 1000),
+            type: "car7",
+          },
+        ]);
+      } catch (error) {
+        console.error("Lỗi tính quãng đường:", error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
 
   return (
     <View style={styles.container}>

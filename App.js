277d8393a32678907a033fe26d67fd73e7219e00
Phuoc Dinh <phuocdinh462@gmail.com>
2023-09-04 22:43:05 +0700

    chore: remove firebase

--- App.js
+++ App.js
@@ -1,72 +1,20 @@
-import { SafeAreaView, Alert, View } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 import { MainStackNavigator } from "./src/navigation/StackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import EStyleSheet from "react-native-extended-stylesheet";
-import messaging from "@react-native-firebase/messaging";
-import { useEffect } from "react";
-import { StatusBar } from "expo-status-bar";
 
 EStyleSheet.build();
 
 export default function App() {
-  async function requestUserPermission() {
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-    if (enabled) {
-      console.log("Authorization status:", authStatus);
-    }
-  }
-
-  useEffect(() => {
-    if (requestUserPermission()) {
-      messaging()
-        .getToken()
-        .then((token) => {
-          console.log(token);
-        });
-    } else {
-      console.log("Failed token status", authStatus);
-    }
-
-    // Check whether an initial notification is available
-    messaging()
-      .getInitialNotification()
-      .then(async (remoteMessage) => {
-        if (remoteMessage) {
-          console.log("Notification caused app to open from quit state:", remoteMessage.notification);
-        }
-      });
-
-    // Assume a message-notification contains a "type" property in the data payload of the screen to open
-    messaging().onNotificationOpenedApp(async (remoteMessage) => {
-      console.log("Notification caused app to open from background state:", remoteMessage.notification);
-    });
-
-    // Register background handler
-    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-      console.log("Message handled in the background!", remoteMessage);
-    });
-
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
-    });
-
-    return unsubscribe;
-  }, []);
-
   return (
     <Provider store={store}>
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <NavigationContainer>
           <MainStackNavigator />
         </NavigationContainer>
-      </View>
-      <StatusBar style="dark" />
+      </SafeAreaView>
     </Provider>
   );
 }
